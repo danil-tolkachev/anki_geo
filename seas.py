abf5b9bbe01add38f8cbea33268278a392acdc0c
Толкачев Данил Сергеевич <tolkachev@nkbvs.tsure.ru>
2018-08-18 17:14:18 +0300

    add straits

--- seas.py
+++ seas.py
@@ -76,6 +76,40 @@ def parse_seas(url, countries, f=sys.stdout):
             file=f)
 
 
+def parse_straits(url, countries, f=sys.stdout):
+    s = get_data(url)
+    tree = html.fromstring(s)
+    first = True
+    for row in tree.xpath("//table/tr"):
+        if first:
+            first = False
+            continue
+        cols = row.xpath(".//td")
+        name = cols[0].xpath(".//text()")[0].strip()
+        img = cols[0].xpath(".//span/img/@src")[0]
+        fname = img.lstrip("/img").replace("/", "_")
+        load_data(
+            os.path.dirname(url) + "/" + img, os.path.join("data", fname))
+        ids = list(map(country_id, cols[0].xpath(".//img[@class]/@src")))
+        country_names = map(lambda id_: countries[id_], ids)
+        country_names = ", ".join(country_names)
+        length = cols[1].text.strip()
+        # depth = cols[2].text.strip()
+        # width = cols[3].text.strip()
+        join = " и ".join(map(str.strip, cols[4].itertext()))
+        separate = " и ".join(map(str.strip, cols[5].itertext()))
+        ocean = cols[6].text.strip()
+        print(
+            name,
+            f'<img src="{fname}">',
+            country_names,
+            length,
+            join,
+            separate,
+            sep=';',
+            file=f)
+
+
 def main():
     res = parse_countries("https://geo.koltyrin.ru/strany_mira.php")
     res.update(
@@ -85,6 +119,8 @@ def main():
             "https://geo.koltyrin.ru/strany_mira_nepriznannye.php"))
     parse_seas("https://geo.koltyrin.ru/morja.php", res,
                open('seas.txt', 'w', encoding='utf-8'))
+    parse_straits("https://geo.koltyrin.ru/prolivy.php", res,
+                  open('straits.txt', 'w', encoding='utf-8'))
 
 
 if __name__ == "__main__":
